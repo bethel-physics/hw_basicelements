9e4e2753f5782a1a490bedc6a4f66159f3fab668
Julie Hogan <jmhogan@fnal.gov>
2018-01-26 16:40:24 -0600

    format for matlab markup

--- loopmatrixplot.m
+++ loopmatrixplot.m
@@ -1,46 +1,59 @@
 %loopmatrixplot -- fill in this program to complete homework 1
 clear all; help loopmatrixplot;
 
-disp(' ')
-disp('Problem 1 – Consider the equation')
-disp('f = (1/2pi)sqrt(k1k2/m(k1+k2))')
-disp('where k1=50, k2=100, and m=2.')
-disp('First, assign values for k1, k2, and m.')
-disp('Next, assign the value to a new variable f, using the variables')
-disp('defined in the first step.')
+%% Problem 1
+% Consider the equation
+% f = (1/2pi)sqrt(k1k2/m(k1+k2))
+% where k1 = 50, k2 = 100, and m = 2.
+%
+% * First, assign values for k1, k2, and m.
+% * Next, assign the value to a new variable f, using the variables
+% defined in the first step.
 
-disp(' ')
-disp('Problem 2 – The temperature distribution between to concentric')
-disp('tubes and in the radial direction is:')
-disp('T = Tb + [(Ta - Tb)ln(b/r)]/ln(b/a')
-disp('where r is the radius, and a <= r <= b. Given Ta = 0, Tb = 100,')
-disp('a=1, and b=2:')
-disp('Determine the temperature for r=1.00, 1.01, 1.02, …, 1.99,2.00.')
-disp('Plot radial position (r) versus temperature (T) as a solid line.')
-disp('On top of that plot, plot every 10th temperature value as a circle.')
-disp('Label the axes and provide a title for the plot.')
+%% Problem 2
+% The temperature distribution between to concentric
+% tubes and in the radial direction is:
+%
+% T = Tb + [(Ta - Tb)ln(b/r)]/ln(b/a
+%
+% where r is the radius, and a <= r <= b. Given Ta = 0, Tb = 100,
+% a = 1, and b = 2:
+%
+% * Determine the temperature for r = 1.00, 1.01, 1.02, …, 1.99,2.00.
+% * Plot radial position (r) versus temperature (T) as a solid line.
+% * On top of that plot, plot every 10th temperature value as a circle.
+% * Label the axes and provide a title for the plot.
 
-disp(' ')
-disp('Problem 3 – Create two vectors, a row vector whose elements are 2n+1')
-disp('and a column vector whose elements are 3n+2, for n=0,1,…,6.') 
+%% Problem 3
+% Create two vectors, a row vector whose elements are 2n+1
+% and a column vector whose elements are 3n+2, for n = 0,1,…,6. 
 
-disp(' ')
-disp('Problem 4 – Create a 5x5 square matrix whose elements are -1 along')
-disp('the diagonal and 1 everywhere else.  Please define the matrix using')
-disp('any of the following built-in functions: zeros, ones, and diag.')
+%% Problem 4
+% Create a 5x5 square matrix whose elements are -1 along
+% the diagonal and 1 everywhere else.  Please define the matrix using
+% any of the following built-in functions: |zeros|, |ones|, and |diag|.
 
-disp(' ')
-disp('Problem 5 – Given the following system of equations')
-disp('1a + 2b + 3c + 4d = 30')
-disp('2a + 1b + 3c + 4d = 29')
-disp('3a + 1b + 2c + 4d = 27')
-disp('4a + 1b + 2c + 3d = 24')
-disp('determine a, b, c, and d.')
+%% Problem 5
+% Given the following system of equations
+% 
+% 1a + 2b + 3c + 4d = 30
+%
+% 2a + 1b + 3c + 4d = 29
+%
+% 3a + 1b + 2c + 4d = 27
+%
+% 4a + 1b + 2c + 3d = 24
+%
+% determine a, b, c, and d.
 
-disp(' ')
-disp('Problem 6 – The motion of a particular damped oscillator is')
-disp('described mathematically as the product of a decay function G1(t)')
-disp('and a sinusoidal function G2(t):')
-disp('F(t) = G1(t)G2(t) = exp(-t/2)sin(5t)')
-disp('Reproduce the graph shown in the README depicting the decay function')
-disp('G1(t) and the damped oscillatory  motion F(t).')
+%% Problem 6
+% The motion of a particular damped oscillator is
+% described mathematically as the product of a decay function G1(t)
+% and a sinusoidal function G2(t):
+%
+% F(t) = G1(t)G2(t) = exp(-t/2)sin(5t)
+%
+% Reproduce the graph below depicting the decay function
+% G1(t) and the damped oscillatory motion F(t).
+
+
